718a9b67582c37298e4da8880d15585313da4f39
DaFleks <petropoulosalex@gmail.com>
2025-07-04 14:45:47 -0400

    adjusted brand and collapse button on sidebar

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,23 +1,40 @@
 "use client";
 
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarTrigger } from "./ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+  useSidebar,
+} from "./ui/sidebar";
 
 import { dashboard, orders, products, customers, utilities } from "../lib/MenuItems";
 import AppSidebarGroup from "./AppSidebarGroup";
 import { Button } from "./ui/button";
-import { Flame } from "lucide-react";
+import { Flame, MoveLeft, PanelLeft } from "lucide-react";
 import Link from "next/link";
 
 const AppSidebar = () => {
+  const { open, toggleSidebar } = useSidebar();
+  
   return (
-    <Sidebar className="font-semibold" collapsible="icon">
-      <SidebarHeader className="p-4 select-none bg-slate-800 text-white">
-        <SidebarContent>
-          <Link href="#" className="flex gap-1 items-center">
-            <Flame size={18} />
-            <h6 className="text-xl font-bold mt-1">Shahp</h6>
-          </Link>
-          <SidebarTrigger className="z-50 absolute top-5 right-2 cursor-pointer" />
+    <Sidebar className="font-bold" collapsible="icon">
+      <SidebarHeader className="select-none bg-slate-800 text-white">
+        <SidebarContent className="flex-row items-center justify-between overflow-hidden">
+          {open && (
+            <Link href="#" className="flex gap-1 items-center">
+              <Flame size={18} />
+              <h6 className="text-xl font-bold mt-1">Shahp</h6>
+            </Link>
+          )}
+          <SidebarMenuButton onClick={toggleSidebar} className="w-fit cursor-pointer mt-1">
+            <PanelLeft />
+          </SidebarMenuButton>
         </SidebarContent>
       </SidebarHeader>
       <SidebarContent className="bg-slate-800 text-white">
